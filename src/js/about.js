6d6089042665291bfbb1c2beacc69343c813ae26
MinaDakiri <minadakiri7@gmail.com>
2020-08-31 16:17:52 +0100

    Add DOM for about

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,8 @@
+//* ***********  DOM  Start ***********
 
+const about = document.querySelector('.profile')
+const skills = document.querySelector('.skills')
+const aboutBtn = document.querySelector('.profile__aboutBtn')
+const skillsBtn = document.querySelector('.profile__skillsBtn')
+
+//* *********** DOM END ***********
