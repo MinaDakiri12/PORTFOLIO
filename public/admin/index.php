911ca9f6fd7cfad08177ca6ed84eee3e7ed79ad7
MinaDakiri12 <minadakiri7@gmail.com>
2020-09-04 16:22:38 +0100

    Restruction files for back end

---
+++ public/admin/index.php
@@ -0,0 +1,33 @@
+<?php 
+require_once('../../resources/config.php');
+include("user_auth.php");
+include(TEMPLATE_BACK . DS . "head.php");
+?>
+
+<?php include(TEMPLATE_BACK . DS . "nav.php") ?>
+<?php include(TEMPLATE_BACK . DS . "ui_settings.php") ?>
+
+<div class="app-main">
+    <?php include(TEMPLATE_BACK . DS . "sidebar.php") ?>
+    <div class="app-main__outer">
+
+        <?php 
+        $access_admin = 1;
+        $access_editor = 2;
+        $access_viewer = 3;
+        if($_SERVER['REQUEST_URI'] == "/admin/" || $_SERVER['REQUEST_URI'] == "/admin/index.php?loginSuccess" || $_SERVER['REQUEST_URI'] == "/admin/index.php"){
+            include(TEMPLATE_BACK . DS . "dashboard.php") ;
+        }
+
+
+        // if(isset($_GET['create_newsC2'])){
+        //     include(TEMPLATE_BACK . DS . "newsC2/create_newsC2.php");
+        // }
+
+
+
+
+
+?>
+
+        <?php include( TEMPLATE_BACK . DS . "footer.php" )?>
