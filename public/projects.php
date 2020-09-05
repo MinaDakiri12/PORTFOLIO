c70ce0dad66b80794643cbd2645346cb78ebd323
MinaDakiri <minadakiri7@gmail.com>
2020-09-05 17:31:44 +0100

    Add function display in projects.php

--- public/projects.php
+++ public/projects.php
@@ -12,40 +12,8 @@
     <div class="projects__container">
 		<h2 class="projects__title">My projects<span class="theme_color">.</span></h2>
 		<div class="projects__wrapper">
-        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
-                <div class="projects__card--top">
-                    <img class="cover" src="images/project3.png "alt="">
-                </div>
-                <div class="projects__card--lower">
-                        <a href=""><h1>Ibtikar Arab Agency Website</h1></a>
-                    <h2>GitHub:</h2>
-                    <a class="projects__github" href="https://github.com/Zineb112/2AI-project">https://github.com/Zineb112/2AI-project</a>
-                    <a class="projects__link" href="">website link</a>
-                </div>
+        <?php display_projects() ?>
         </div>
-        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
-                <div class="projects__card--top">
-                    <img class="cover" src="images/project2.png" alt="">
-                </div>
-                <div class="projects__card--lower">
-                        <a href=""><h1>Covid Test Website</h1></a>
-                    <h2>GitHub:</h2>
-                    <a class="projects__github" href="https://coronatest19.netlify.app/">https://github.com/MinaDakiri12/Project-Corona-</a>
-                    <a class="projects__link" href="">website link</a>
-                </div>
-        </div>
-        <div class="projects__card" data-aos="flip-left" data-aos-duration="1200">
-                    <div class="projects__card--top">
-                        <img class="cover" src="images/project 1.png" alt="">
-                    </div>
-                    <div class="projects__card--lower">
-                            <a href=""><h1> Agency Nexter</h1></a>
-                        <h2>GitHub:</h2>
-                        <a class="projects__github" href="https://agence-immobilier.netlify.com/">https://github.com/Zineb112/Projects</a>
-                        <a class="projects__link" href="">website link</a>
-					</div>
-        </div>
-    </div>
     </div>
 </section>
 <!--End Projects-->
