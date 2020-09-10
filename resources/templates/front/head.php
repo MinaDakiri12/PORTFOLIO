2b62d10cf0adb2224e9b128b2496f8f61d420925
MinaDakiri <minadakiri7@gmail.com>
2020-09-10 13:38:16 +0100

    Add logo in head

--- resources/templates/front/head.php
+++ resources/templates/front/head.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-        <link rel="icon" type="image/png" href="assets/images/logo.png"/>
+        <link rel="icon" type="image/png" href="assets/images/logo-mina.png"/>
 
         <link rel="stylesheet" href="assets/css/style.css">
         <link rel="stylesheet" href="assets/css/owl-carousel/owl.carousel.min.css">
